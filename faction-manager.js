faccdb2e237b19b0b8a0c8e24fc660e29545366c
ResidentIvo <ivo.cegantini@gmail.com>
2026-05-26 14:06:09 -0300

    fix: getFavorToDonate is standalone, not under singularity ns

--- faction-manager.js
+++ faction-manager.js
@@ -144,7 +144,7 @@ export async function main(ns) {
     while (dataRetries-- > 0) {
         try {
             log(ns, 'Getting all faction data...' + (dataRetries < 2 ? ` (attempt ${3 - dataRetries}/3)` : ''));
-            favorToDonate = await getNsDataThroughFile(ns, 'ns.singularity.getFavorToDonate()', '/Temp/favor-to-donate.txt');
+            favorToDonate = await getNsDataThroughFile(ns, 'ns.getFavorToDonate()', '/Temp/favor-to-donate.txt');
             await updateFactionData(ns, allFactions, omitFactions);
             log(ns, 'Getting all augmentation data...');
             await updateAugmentationData(ns, desiredAugs);
