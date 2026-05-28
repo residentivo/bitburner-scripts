3380d63e96dd9a4b613335868863a45c68c5c00e
ResidentIvo <ivo.cegantini@gmail.com>
2026-05-28 20:46:32 -0300

    fix: guard against undefined XP farm target

--- daemon.js
+++ daemon.js
@@ -671,7 +671,9 @@ async function doTargetingLoop(ns) {
                 let time = await farmHackXp(ns, 1.00, verbose);
                 loopInterval = Math.min(1000, time || 1000); // Wake up earlier if we're almost done an XP cycle
             } else if (!isWorkCapped() && lowUtilizationIterations > 10) {
-                let expectedRunTime = getXPFarmTarget().timeToHack();
+                let xpFarmTarget = getXPFarmTarget();
+                if (!xpFarmTarget) return ns.print("WARN: No XP farm target available");
+                let expectedRunTime = xpFarmTarget.timeToHack();
                 let freeRamToUse = (expectedRunTime < loopInterval) ? // If expected runtime is fast, use as much RAM as we want, it'll all be free by our next loop.
                     1 - (1 - lowUtilizationThreshold) / (1 - utilizationPercent) : // Take us just up to the threshold for 'lowUtilization' so we don't cause unecessary server purchases
                     1 - (1 - maxUtilizationPreppingAboveHackLevel - 0.05) / (1 - utilizationPercent); // Otherwise, leave more room (e.g. for scheduling new batches.)
